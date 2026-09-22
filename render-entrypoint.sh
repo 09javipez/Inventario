9bceb109fb3402d6f0d512f350a3bfaadffb82db
09javipez <87841893+09javipez@users.noreply.github.com>
2026-09-22 11:26:19 -0500

    Apregar configuración para render

---
+++ render-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+php artisan config:clear
+
+echo "Running database migrations..."
+php artisan migrate --force
+
+echo "Linking storage..."
+php artisan storage:link || true
+
+echo "Caching config/routes/views..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Starting Apache..."
+exec apache2-foreground
